cd4dd3118df7353b1c931b86cfd2eb7a8ab3d796
Adeilson Silva <adeilson@gmail.com>
2019-08-09 23:39:58 -0300

    - Funcionalidades avancadas web socket

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css';
 
@@ -7,9 +8,11 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main ({ match }) {
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
   useEffect(() => {
     async function loadUsers () {
@@ -24,6 +27,30 @@ export default function Main ({ match }) {
 
     loadUsers();
   }, [match.params.id]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+      query: { user: match.params.id }
+    });
+
+    socket.on('match', dev => {
+      setMatchDev(dev);
+    });
+  }, [match.params.id]);
+
+  // useEffect(() => {
+  //   const socket = io('http://localhost:3333');
+
+  //   socket.on('world', message => {
+  //     console.log('message :', message);
+  //   });
+
+  //   setTimeout(() => {
+  //     socket.emit('hello', {
+  //       message: 'Hello World'
+  //     });
+  //   }, 3000);
+  // }, [match.params.id]);
 
   async function handleLike (id) {
     await api.post(`/devs/${id}/likes`, {}, {
@@ -52,7 +79,6 @@ export default function Main ({ match }) {
       </Link>
       { users.length > 0 ? (
         <ul>
-
           {
             users.map(user => (
               <li key={user._id}>
@@ -76,6 +102,17 @@ export default function Main ({ match }) {
         </ul>
       ) : (
         <div className='empty'>Acabou :(</div>
+      )}
+
+      {matchDev && (
+        <div className='match-container'>
+          <img src={itsamatch} alt={`It's a match`} />
+          <img className='avatar' src={matchDev.avatar} alt='' />
+          <strong>{matchDev.name}</strong>
+          <p>{matchDev.bio}</p>
+
+          <button type='button' onClick={() => setMatchDev(null)}>FECHAR</button>
+        </div>
       )}
     </div>
   );
